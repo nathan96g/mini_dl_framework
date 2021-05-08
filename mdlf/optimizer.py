9b13a4f869ada84a132528d63637ded347cf3902
Julien Sven Adda <julien.adda@epfl.ch>
2021-05-08 16:07:30 +0200

    SGD full step random

--- mdlf/optimizer.py
+++ mdlf/optimizer.py
@@ -13,21 +13,24 @@ class SGD(Optimizer):
 
     def step(self, model, train_data, train_label):
         #when step is called, need to iterate on all the data points for 1 epochs
-
+        random_int = empty(1000).uniform_(0, 1).sort().indices
+        
         #TODO: take random element without any other import: https://discuss.pytorch.org/t/efficiently-selecting-a-random-element-from-a-vector/37757
-        train_sample = train_data[0]
-        train_sample_label = train_label[0]
-        model.backward(train_sample, train_sample_label)
-        for module in model.modules :
-            #Does not do if module is a Identity Activation or Identity Layer
-            if not (issubclass(type(module),activations.Identity) or issubclass(type(module),layers.Identity)) :
-                updates = []
-                for param in module.param():
-                    weight = param[0]
-                    weight_grad = param[1]
-                    updates.append(weight + self.lambda_ * weight_grad)
-                module.update(updates)
-        return model
+        for n in range(1000) :
+            train_sample= train_data[random_int[n]]
+            train_sample_label= train_label[random_int[n]]
+            
+            model.backward(train_sample, train_sample_label)
+            for module in model.modules :
+               #Does not do if module is a Identity Activation or Identity Layer
+                if not (issubclass(type(module),activations.Identity) or issubclass(type(module),layers.Identity)) :
+                   updates = []
+                   for param in module.param():
+                       weight = param[0]
+                       weight_grad = param[1]
+                       updates.append(weight + self.lambda_ * weight_grad)
+                   module.update(updates)
+            return model
 
 #TODO : see if time to implement => require to store list of grad in modules and not just grad
 class minibatch_SGD(Optimizer):
